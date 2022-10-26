016cd4ae6ead70e24b714dc78de87a937082866b
rock0 <yuliia.prysiazhna@pollub.edu.pl>
2022-10-26 19:17:50 +0200

    Wykonanie 3 zadań z przykładowymi wynikami

---
+++ src/main/java/pwo/app/Sequence.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pwo.app;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+        (new SeqToFileApp()).run(args);
+    }
+}
---
+++ src/main/java/pwo/utils/SequenceGenerator.java
@@ -0,0 +1,9 @@
+package pwo.utils;
+
+ import java.math.BigDecimal;
+
+ public interface SequenceGenerator {
+ void reset();
+ BigDecimal nextTerm();
+ BigDecimal getTerm(int i);
+ }
